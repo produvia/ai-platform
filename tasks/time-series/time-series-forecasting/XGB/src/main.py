2e8e6c2b1280afc8424c8b7bdbdc50d66846bdff
Mine Melodi Caliskan <minemelodicaliskan@gmail.com>
2019-07-09 10:37:06 +0300

    click commands

--- tasks/time-series/time-series-forecasting/XGB/src/main.py
+++ tasks/time-series/time-series-forecasting/XGB/src/main.py
@@ -1,11 +1,16 @@
 from runner import ForecastRunner
-import sys
+import click
 
 
-def main():
-    url = sys.argv[1]
-    output_file = sys.argv[2]
-    predicted_date = sys.argv[3]
+URL = 'https://data.smartdublin.ie/dataset/10130831-d2a5-4f4f-b56b-03d8e2cc56c8/resource/' \
+    '6091c604-8c94-4b44-ac52-c1694e83d746/download/dccelectricitycivicsblocks34p20130221-1840.csv'
+
+
+@click.command()
+@click.option("--url", default=URL)
+@click.option("--output_file", default='output/output.csv')
+@click.option("--predicted_date", default='2013-01-07')
+def main(url, output_file, predicted_date):
     model = ForecastRunner(url, output_file, predicted_date)
     df_test = model.fit()
     model.predict(df_test)
