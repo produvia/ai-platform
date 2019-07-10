1df0fa03d5bd17dd2c92617a25c6e4ca3a9086d4
Mine Melodi Caliskan <minemelodicaliskan@gmail.com>
2019-07-10 14:15:07 +0300

    update main for mlflow

--- tasks/time-series/time-series-forecasting/XGB/src/main.py
+++ tasks/time-series/time-series-forecasting/XGB/src/main.py
@@ -1,27 +1,18 @@
 from runner import ForecastRunner
-import click
+import mlflow
+import sys
 
-
-URL = 'https://data.smartdublin.ie/dataset/10130831-d2a5-4f4f-b56b-03d8e2cc56c8/resource/' \
-    '6091c604-8c94-4b44-ac52-c1694e83d746/download/dccelectricitycivicsblocks34p20130221-1840.csv'
-
-
-@click.command()
-@click.option("--url", default=URL)
-@click.option("--output_file", default='../output/output.csv')
-@click.option("--predicted_date", default='2013-01-07')
-@click.option("--min_child_weight", default=[0.8,1.2])
-@click.option("--colsample_bytree", default=[0.9])
-@click.option("--max_depth", default=[3, 4, 6])
-@click.option("--n_estimators", default=[500, 1000])
-@click.option("--eval_metric", default=['rmse'])
-def main(url, output_file, predicted_date, min_child_weight, colsample_bytree, max_depth, n_estimators,
-                 eval_metric):
-    model = ForecastRunner(url, output_file, predicted_date, min_child_weight, colsample_bytree, max_depth, n_estimators,
-                 eval_metric)
-    df_test = model.fit()
-    model.predict(df_test)
-
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    url = sys.argv[1]
+    output_file = sys.argv[2]
+    predicted_date = sys.argv[3]
+    min_child_weight = eval(sys.argv[4])
+    colsample_bytree = eval(sys.argv[5])
+    max_depth = eval(sys.argv[6])
+    n_estimators = eval(sys.argv[7])
+    eval_metric = sys.argv[8]
+    with mlflow.start_run():
+        model = ForecastRunner(url, output_file, predicted_date, min_child_weight, colsample_bytree, max_depth,
+                               n_estimators, eval_metric)
+        df_test = model.fit()
+        model.predict(df_test)
