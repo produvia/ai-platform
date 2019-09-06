01d60ab379fb9efb61cdfff17ddb46f189ddbd45
= <smuralid@sfu.ca>
2019-09-05 22:47:17 -0700

    cifar with vgg

--- tasks/computer-vision/image-classification/main.py
+++ tasks/computer-vision/image-classification/main.py
@@ -47,7 +47,7 @@ if args.cuda:
 cifar_train_mean = (0.4914, 0.4822, 0.4465)
 cifar_train_std = (0.2023, 0.1994, 0.2010)
 train_loader = torch.utils.data.DataLoader(
-    datasets.CIFAR100('../data', train=True, download=True,
+    datasets.CIFAR100('./data', train=True, download=True,
     				transform=transforms.Compose([
                     transforms.RandomCrop(32, padding=4),
                     transforms.RandomHorizontalFlip(),
@@ -58,7 +58,7 @@ train_loader = torch.utils.data.DataLoader(
 
 
 test_loader = torch.utils.data.DataLoader(
-    datasets.CIFAR100('../data', train=False, download=True,
+    datasets.CIFAR100('./data', train=False, download=True,
     				transform=transforms.Compose([
                     transforms.ToTensor(),
     				transforms.Normalize(cifar_train_mean,cifar_train_std)
