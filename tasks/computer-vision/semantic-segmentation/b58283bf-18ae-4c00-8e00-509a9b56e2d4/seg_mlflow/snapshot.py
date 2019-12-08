5c94352381626b33a4d78bafc99916f50f330021
Mohamed Laradji <malaradji@gmail.com>
2019-12-08 02:10:54 -0800

    integrate(task(netik1020/object_segmentation:b58283bf-18ae-4c00-8e00-509a9b56e2d4)): assign uuid

--- tasks/computer-vision/semantic-segmentation/b58283bf-18ae-4c00-8e00-509a9b56e2d4/seg_mlflow/snapshot.py
+++ tasks/computer-vision/semantic-segmentation/b58283bf-18ae-4c00-8e00-509a9b56e2d4/seg_mlflow/snapshot.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import os
 
 import keras.callbacks as callbacks
 from keras.callbacks import Callback
+
 
 class SnapshotModelCheckpoint(Callback):
     """Callback that saves the snapshot weights of the model.
@@ -16,17 +18,17 @@ class SnapshotModelCheckpoint(Callback):
         fn_prefix: prefix for the filename of the weights.
     """
 
-    def __init__(self, nb_epochs, nb_snapshots, fn_prefix='Model'):
+    def __init__(self, nb_epochs, nb_snapshots, fn_prefix="Model"):
         super(SnapshotModelCheckpoint, self).__init__()
 
         self.check = nb_epochs // nb_snapshots
         self.fn_prefix = fn_prefix
 
-    def on_epoch_end(self, epoch, logs={}):
+    def on_epoch_end(self, epoch):
         if epoch != 0 and (epoch + 1) % self.check == 0:
             filepath = self.fn_prefix + "-%d.h5" % ((epoch + 1) // self.check)
             self.model.save_weights(filepath, overwrite=True)
-            #print("Saved snapshot at weights/%s_%d.h5" % (self.fn_prefix, epoch))
+            # print("Saved snapshot at weights/%s_%d.h5" % (self.fn_prefix, epoch))
 
 
 class SnapshotCallbackBuilder:
@@ -48,7 +50,7 @@ class SnapshotCallbackBuilder:
         self.M = nb_snapshots
         self.alpha_zero = init_lr
 
-    def get_callbacks(self, model_prefix='Model'):
+    def get_callbacks(self, model_prefix="Model"):
         """
         Creates a list of callbacks that can be used during training to create a
         snapshot ensemble of the model.
@@ -57,18 +59,28 @@ class SnapshotCallbackBuilder:
         Returns: list of 3 callbacks [ModelCheckpoint, LearningRateScheduler,
                  SnapshotModelCheckpoint] which can be provided to the 'fit' function
         """
-        if not os.path.exists('weights/'):
-            os.makedirs('weights/')
+        if not os.path.exists("weights/"):
+            os.makedirs("weights/")
 
-        callback_list = [callbacks.ModelCheckpoint("weights/%s-Best.h5" % model_prefix, monitor="val_acc",
-                                                    save_best_only=True, save_weights_only=True),
-                         callbacks.LearningRateScheduler(schedule=self._cosine_anneal_schedule),
-                         SnapshotModelCheckpoint(self.T, self.M, fn_prefix='weights/%s' % model_prefix)]
+        callback_list = [
+            callbacks.ModelCheckpoint(
+                "weights/%s-Best.h5" % model_prefix,
+                monitor="val_acc",
+                save_best_only=True,
+                save_weights_only=True,
+            ),
+            callbacks.LearningRateScheduler(schedule=self._cosine_anneal_schedule),
+            SnapshotModelCheckpoint(
+                self.T, self.M, fn_prefix="weights/%s" % model_prefix
+            ),
+        ]
 
         return callback_list
 
     def _cosine_anneal_schedule(self, t):
-        cos_inner = np.pi * (t % (self.T // self.M))  # t - 1 is used when t has 1-based indexing.
+        cos_inner = np.pi * (
+            t % (self.T // self.M)
+        )  # t - 1 is used when t has 1-based indexing.
         cos_inner /= self.T // self.M
         cos_out = np.cos(cos_inner) + 1
         return float(self.alpha_zero / 2 * cos_out)
